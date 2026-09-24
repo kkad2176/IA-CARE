50cbd74897b072bd4c96244022fdb7bcdbee5e45
kkad2176 <kkad2176@gmail.com>
2026-09-24 15:02:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7432,7 +7432,7 @@ if resultats:
         creer_prescription_ide = False
 
       
-        if avk_detecte 
+        if avk_detecte:
 
             creer_ordonnance_pharmacie = st.checkbox(
                 "Créer l'ordonnance pharmacie",
